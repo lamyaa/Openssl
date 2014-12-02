84282a771cc0d5969803073b5c045d94d9ed405e
lamyaa <eloussi2@illinois.edu>
2014-12-02 17:13:57 -0600

    changing coverage script to put .gcov ext to .c

--- coverage.sh
+++ coverage.sh
@@ -2,14 +2,19 @@
 
 sum=0
 avg=0
+currentdir=`pwd`
 for f in $(find -name *.gcda)
 do
-    line=`gcov $f | grep -m1 "Lines executed"`
+    file=`echo $f | rev | cut -d'/' -f 1 | rev`
+    dir=`echo $f | rev | cut -d'/' -f2- | rev`
+    cd $dir
+    line=`gcov $file | grep -m1 "Lines executed"`
     percent=`echo $line | grep -oE '[0-9]+\.[0-9]+'`
     total=`echo $line | grep -oE '[0-9]+$'`
     let "sum += total"
     res=`echo "$total * $percent/100" | bc -l`
     avg=`echo "$avg + $res" | bc -l`
+    cd $currentdir
 done
 
 avg=`echo "$avg/$sum" | bc -l`
